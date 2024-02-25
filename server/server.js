10531190911492563a41879ba198afe37b514557
Koboil <hcousin2@myges.fr>
2024-02-25 20:13:40 +0100

    connexion a la bdd

--- server/server.js
+++ server/server.js
@@ -4,8 +4,49 @@ const io = require('socket.io')(3000, {
         methods: ["GET", "POST", "PATCH"]
     }
 })
+const { Client } = require('pg');
 
-const rooms = []
+
+// Database connection configuration
+const dbConfig = {
+  user: 'root',
+  password: 'root',
+  host: 'localhost',
+  port: 5432,
+  database: 'database',
+};
+
+// Create a new PostgreSQL client
+const client = new Client(dbConfig);
+
+// Connect to the database
+client.connect()
+  .then(() => {
+    console.log('Connected to PostgreSQL database');
+
+    // Execute SQL queries here
+
+    client.query('SELECT * FROM employees', (err, result) => {
+      if (err) {
+        console.error('Error executing query', err);
+      } else {
+        console.log('Query result:', result.rows);
+      }
+
+      // Close the connection when done
+      client.end()
+        .then(() => {
+          console.log('Connection to PostgreSQL closed');
+        })
+        .catch((err) => {
+          console.error('Error closing connection', err);
+        });
+    });
+  })
+  .catch((err) => {
+    console.error('Error connecting to PostgreSQL database', err);
+  });
+
 
 io.on("connection", (socket) => {
     console.log("A user connected")
